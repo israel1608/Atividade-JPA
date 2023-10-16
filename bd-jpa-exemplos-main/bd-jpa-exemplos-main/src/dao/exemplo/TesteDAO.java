0f32cadc1a1f40e8daa545a6311e290001da3ee4
Israel Costa <81455159+israel1608@users.noreply.github.com>
2023-10-16 09:38:00 -0300

    Update TesteDAO.java

--- bd-jpa-exemplos-main/bd-jpa-exemplos-main/src/dao/exemplo/TesteDAO.java
+++ bd-jpa-exemplos-main/bd-jpa-exemplos-main/src/dao/exemplo/TesteDAO.java
@@ -1,5 +1,7 @@
 package dao.exemplo;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -12,19 +14,35 @@ public class TesteDAO {
 
 	public static void main(String[] args) {
 		DAO dao = new EmprestimoDAO();
-		EntityManager em = dao.getEntityManager();
+		EntityManager em = dao.getEntityManager(); // Entidade gerenciadora 
+		
+		Emprestimo p = new Emprestimo(2,5);  //parâmetros para emprestimo id aluno, codigo publicação
+		
+		dao.insert(p);  // Função insert() 
+		
+	  //dao.delete(p); descomente para testar a função DELETE !!!
 
-		realizarEmprestimo(em);
-		// altera(em);
+		imprimirEmprestimos(dao); //Função FindAll() -----
+		
 		em.close();
 	}
+	
+	public static void imprimirEmprestimos(DAO dao) {
+	    List<Emprestimo> emprestimos = dao.findAll();
 
-	private static void realizarEmprestimo(EntityManager em) {
-		EntityTransaction transaction = em.getTransaction();
-		transaction.begin();
-		Emprestimo p = new Emprestimo(4,2);
-		em.persist(p);
-		transaction.commit();
+	    if (emprestimos.isEmpty()) {
+	        System.out.println("Nenhum empréstimo encontrado.");
+	    } else {
+	        System.out.println("Lista de Empréstimos:");
+	        for (Emprestimo emprestimo : emprestimos) {
+	            System.out.println("ID: " + emprestimo.getId());
+	            System.out.println("codigo aluno: " + emprestimo.getAlunoId()); 
+	            System.out.println("codigo livro: " + emprestimo.getCodigoPub()); 
+	            System.out.println("Data emprestimo: " + emprestimo.getData_emprestimo());
+	            System.out.println();
+	        }
+	    }
 	}
+
 
 }
